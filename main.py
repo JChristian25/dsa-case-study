d7c9201d0305e8bee4e8a095882c689ce3f0440d
jeop-qrs <jeofhernandez2@gmail.com>
2025-11-06 01:28:03 +0800

    added logic validation

--- main.py
+++ main.py
@@ -46,6 +46,19 @@ def main():
     # 2. TRANSFORM (Jeoffrey's Task)
     print("Calculating weighted grades...")
     grades = compute_weighted_grades(all_students, config['grade_weights'])
+    # # Checking if the logic of compute_weighted_grades is working correctly
+    # print(f"{"=" * 5} LIST OF STUDENTS {"=" * 5}")
+    # print(f"{'Student Name':19} {'Weighted Grade'}")
+    # for stud in grades:
+    #     print(f"{stud['first_name'] + ' ' + stud['last_name']:23} {stud['weighted_grade']:2.2f}")
+
+    print("Calculating distribution...")
+    distribution = calculate_distribution(grades, config['thresholds']['grade_letters'])
+    # # Checking if the logic of calculate_distribution is working correctly
+    # print(f"Total students: {len(all_students)}")
+    # print(f"{"=" * 5} GRADE LETTER COUNT {"=" * 5}")
+    # for key, val in distribution.items():
+    #     print(f"{key + ':':^4} {val:^4} {'*' * val}")
 
     # 3. ANALYZE (Jeoffrey, Miles, JC's Tasks)
     print("Analyzing data...")
@@ -55,7 +68,7 @@ def main():
     
     print("\n--- Academic Insights ---")   
     
-    print(f"\nSuccessfully exported at-risk report to {OUTPUT_REPORT_PATH}")
+    # print(f"\nSuccessfully exported at-risk report to {OUTPUT_REPORT_PATH}")
 
 
 if __name__ == "__main__":
