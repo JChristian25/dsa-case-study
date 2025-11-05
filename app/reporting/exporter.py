feab96e68ed2c8685dd9bb52f03f3197fb1df2f0
yyxy0 <danielmicompal6@gmail.com>
2025-11-05 22:36:52 +0800

    Update exporter.py

--- app/reporting/exporter.py
+++ app/reporting/exporter.py
@@ -2,5 +2,15 @@
 import csv
 from typing import Any, Dict, List
 
-def export_to_csv(data: List[Dict[str, Any]], filepath: str, fieldnames: List[str]):
-    pass
+import csv
+
+students_csv_data = []
+
+def export_to_csv(data: List[Dict[str, Any]], filepath: str):
+    with open(filepath, mode="w", newline="", encoding="utf-8") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=data[0].keys())
+        writer.writeheader()     
+        writer.writerows(students_csv_data) 
+    print(f"Data exported successfully to {filepath}")
+
+export_to_csv(students_csv_data,"test/students_output.csv")
